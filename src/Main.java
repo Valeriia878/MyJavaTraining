b714726f0c597ca357459eb93142acebee55e440
Valeriia <vshepil888@gmail.com>
2023-03-07 16:46:17 +0200

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int first = 5;
+        int second = first * 5;
+        int third = second - first;
+        first = first * third;
+        System.out.println(first + "," + second + "," + third);
+        int x = first + second;
+        System.out.println(x);
+        int y = third%first;
+        System.out.println(y);
     }
 }
